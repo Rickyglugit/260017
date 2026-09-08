65bbfb2400e7097eb7ce466b41a044cd63544038
Rickyglugit <Ricky.biezen@gmail.com>
2026-09-08 12:30:04 +0200

    verder gewerkt aan week opdracht

--- week1-hello world/sketch.js
+++ week1-hello world/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+  textSize(15)
+  text("1 Ricky Biezen",50,50);
+
+  text("2 vlag",50,100);
+  fill(255,0,0);
+rect( 50, 150, 150,50);
+
+fill(255,255,255,)
+rect(50,150,150,50)
 }
