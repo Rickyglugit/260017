f36238ecdd0462f7afd18e39523dd317286c09f8
Rickyglugit <Ricky.biezen@gmail.com>
2026-09-08 10:51:35 +0200

    change

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,9 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
-}line(x1, y2, x1, y2,
+  createCanvas(800, 800);
+  console.log("hello world")
+}
 
-)
+
 
 function draw() {
-  background(220);
+  background("blue");
+  console.log("hello world");
+  circle(50,50,50,50);
+  rect(40,70,20,50);
+  line(40,80,10,10);
+  line(60,80,80,-20);
+  ellipse(40,50,10,20);
+  ellipse(60,50,10,20);
+  line()
 }
+
---
+++ lege sketch copy/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(220);
+rect(40,60,100,300);
+rect(60,350,25,150);
+}
