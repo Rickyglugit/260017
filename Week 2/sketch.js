29199b409e670ed06282a78165bc45c8a71ab2dd
Rickyglugit <Ricky.biezen@gmail.com>
2026-09-14 10:29:14 +0200

    start aan week 2

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
 function draw() {
   background(220);
+  strokeWeight(0);
+  fill(180, 255, 252);
+  rect(0,0,800,600);
+  
+  fill(115, 115, 99);
+  rect(0,500,800,100);
+
+  fill(235, 235, 235);
+  rect(-25,530,100,20,20);
+
+  rect(150,530,100,20,20);
 }
