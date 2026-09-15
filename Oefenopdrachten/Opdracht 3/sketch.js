72f3dea873db94b90fa56ca4a7a213d9c67abaf7
Rickyglugit <Ricky.biezen@gmail.com>
2026-09-15 10:43:13 +0200

    gewerkt aan oefen opdracht 3 en week 2

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,26 @@
+let score = 95
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
 function draw() {
   background(220);
+  
+  fill(0,0,0);
+  if(score>= 90){
+    text("uitstekend",20,20);
+  }
+  else if(score>= 70 && score < 89){ 
+    text("goed gedaan",20,20);
+  }
+  else if(score>= 50 && score < 69){
+    text("voldoende",20,20);
+  }
+  else 
+  fill(200,0,0);
+    text("onvoldoende",20,20);
+  
+  
 }
