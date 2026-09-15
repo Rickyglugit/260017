7a5e3a303f5181e46371b8d27a402866def177e7
Rickyglugit <Ricky.biezen@gmail.com>
2026-09-15 11:27:45 +0200

    finished oefenopdracht 3

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -3,6 +3,7 @@ let score = 95
 
 function setup() {
   createCanvas(400, 200);
+  score = random(0,100)
 }
 
 function draw() {
@@ -10,17 +11,21 @@ function draw() {
   
   fill(0,0,0);
   if(score>= 90){
-    text("uitstekend",20,20);
+    text("uitstekend",20,20); 
+    console.log("scoren is gelijk of groter dan 90")
   }
   else if(score>= 70 && score < 89){ 
     text("goed gedaan",20,20);
+    console.log("scoren is 70 tot en met 89")
   }
   else if(score>= 50 && score < 69){
     text("voldoende",20,20);
+    console.log("scoren is 50 tot en met 69")
   }
-  else 
-  fill(200,0,0);
+  else {
+    fill(200,0,0);
     text("onvoldoende",20,20);
+  }
   
   
 }
