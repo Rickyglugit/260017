6c41193d8c0bfb77650627acc5b7371656783765
Rickyglugit <Ricky.biezen@gmail.com>
2026-09-21 12:45:46 +0200

    update
    start gemaakt aan week 3 en opdracht 4 afgerond

--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -1,10 +1,14 @@
 /// variabl opdr2
 let teller = 1
 // vari opdr 3
-let trafficlight = 'black'
-let circlelight1 = 'black'
-let circlelight2 = 'black'
-let circlelight3 = 'black'
+let trafficlight = 0;
+let circlelight1 = 'red';
+let circlelight2 = 'black';
+let circlelight3 = 'black';
+// variablen opdr 4
+let achtball = 0;
+let eightballXPositie = 500;
+let eightballYPosition = 200;
 
 function setup() {
   createCanvas(800, 575);
@@ -13,6 +17,7 @@ function setup() {
 function draw() {
   background(220);
   fill(0, 0, 0);
+  textSize(10)
   text("1Houd B in om een blokje te laten verschijnen.", 20, 20);
   text("2 Druk op spatie om het getal op 0 te zetten", 20, 120);
   text("3 Druk op enter om van rood -> groen -> oranje te gaan", 20, 240);
@@ -42,12 +47,70 @@ function draw() {
   rect(30, 300, 50, 200);
   rect(45, 500, 20, 70);
   //lichten
-  fill(trafficlight);
+  fill(circlelight1);
   circle(55, 325, 30, 20);
-  fill(trafficlight);
+  fill(circlelight2);
   circle(55, 390, 30, 20);
-  fill(trafficlight);
+  fill(circlelight3);
   circle(55, 460, 30, 20);
+
+  //opdracht4
+  // 8 ball
+  fill(0,0,0);
+  circle(eightballXPositie,eightballYPosition,120);
+  fill(255,255,255);
+  circle(eightballXPositie,eightballYPosition,90)
+  fill(0,0,0);
+  textSize(40)
+  text("8",eightballXPositie-10,eightballYPosition+10);
 
 }
 
+function keyPressed(){
+  //console.log(keyCode);
+
+  // We drukken op enter
+  if(keyCode == ENTER){
+    trafficlight = trafficlight + 1;
+
+    if(trafficlight > 2){
+      trafficlight = 0;
+    }
+
+
+    if (trafficlight == 0){
+      circlelight1 = "red";
+      circlelight2 = "black";
+      circlelight3 = "black";
+    }
+    if(trafficlight ==1){
+       circlelight1 = "black"
+      circlelight2 = "orange"
+      circlelight3 = "black"
+    }
+      
+    if(trafficlight ==2){
+       circlelight1 = "black"
+      circlelight2 = "black"
+      circlelight3 = "green"
+    }
+     
+    console.log(trafficlight);
+
+  }
+
+  // We drukken op A, dus we gaan naar links
+  if (keyCode == 65){
+    eightballXPositie = eightballXPositie - 10;
+    console.log(eightballXPositie);
+  }
+  else if(keyCode == 68){
+    eightballXPositie = eightballXPositie + 10;
+  }
+  else if (keyCode == 87){
+    eightballYPosition = eightballYPosition - 10;
+  }
+  else if(keyCode == 83){
+    eightballYPosition = eightballYPosition + 10;
+  }
+}
--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,23 @@
+let platformkleur = fill()
+let kleinevakkleur = fill()
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1300, 550);
 }
 
 function draw() {
   background(220);
+  // het platform voor de vakjes
+  fill(100,100,100);
+  strokeWeight(3);
+  rect(400, 50, 450, 450, 10);
+  // de 9 vlakjes
+  fill(200,200,200);
+  rect(425,75,125,125,10);
+  smallrectangle(20,20);
+}
+function smallrectangle(){
+  strokeWeight(3)
+  fill(200,200,200);
+  rect(425,75,125,125,10);
 }
