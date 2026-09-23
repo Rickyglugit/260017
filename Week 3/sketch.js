f805605ba3486c5d894006529bafedb48fab65f5
Rickyglugit <Ricky.biezen@gmail.com>
2026-09-23 21:14:03 +0200

    update week3
    heb (player1) win screen toegevoegd voor horizontaal en verticaal

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -38,6 +38,54 @@ function draw() {
   smallrectangle(563, 360, vakje8);
   smallrectangle(700, 360, vakje9);
 
+  if (vakje1 == 1 && vakje2 == 1 && vakje3 == 1) {
+    line(30, 30, 30, 30);
+    fill(200, 0, 0);
+    textSize(150)
+    text("Player 1 wins", 200, 250);
+    fill(255, 0, 0, 127);
+    rect(0, 0, 1300, 550);
+  }
+  if (vakje4 == 1 && vakje5 == 1 && vakje6 == 1) {
+    line(30, 30, 30, 30);
+    fill(200, 0, 0);
+    textSize(150)
+    text("Player 1 wins", 200, 250);
+    fill(255, 0, 0, 127);
+    rect(0, 0, 1300, 550);
+  }
+   if (vakje7 == 1 && vakje8 == 1 && vakje9 == 1) {
+    line(30, 30, 30, 30);
+    fill(200, 0, 0);
+    textSize(150)
+    text("Player 1 wins", 200, 250);
+    fill(255, 0, 0, 127);
+    rect(0, 0, 1300, 550);
+  }
+   if (vakje1 == 1 && vakje4 == 1 && vakje7 == 1) {
+    line(30, 30, 30, 30);
+    fill(200, 0, 0);
+    textSize(150)
+    text("Player 1 wins", 200, 250);
+    fill(255, 0, 0, 127);
+    rect(0, 0, 1300, 550);
+  }
+   if (vakje2 == 1 && vakje5 == 1 && vakje8 == 1) {
+    line(30, 30, 30, 30);
+    fill(200, 0, 0);
+    textSize(150)
+    text("Player 1 wins", 200, 250);
+    fill(255, 0, 0, 127);
+    rect(0, 0, 1300, 550);
+  }
+   if (vakje3 == 1 && vakje6 == 1 && vakje9 == 1) {
+    line(30, 30, 30, 30);
+    fill(200, 0, 0);
+    textSize(150)
+    text("Player 1 wins", 200, 250);
+    fill(255, 0, 0, 127);
+    rect(0, 0, 1300, 550);
+  }
 }
 function smallrectangle(xPos, yPos, eigenaar) {
   strokeWeight(3)
@@ -59,56 +107,50 @@ function smallrectangle(xPos, yPos, eigenaar) {
 
 
 function mousePressed() {
- 
+
   if (mouseButton == LEFT) {
- 
+
     // eerste rij
     if (mouseX > 425 && mouseX < 425 + 125 && mouseY > 70 && mouseY < 70 + 125 && vakje1 == 0) {
       vakje1 = beurt;
       wisselBeurtNaarVolgendeSpeler();
     }
 
-    else if (mouseX > 563 && mouseX < 563 + 125 && mouseY > 70 && mouseY < 70 + 125 && vakje2 ==0) {
+    else if (mouseX > 563 && mouseX < 563 + 125 && mouseY > 70 && mouseY < 70 + 125 && vakje2 == 0) {
       vakje2 = beurt
       wisselBeurtNaarVolgendeSpeler();
     }
-    else if (mouseX > 700 && mouseX < 700 + 125 && mouseY > 70 && mouseY < 70 + 125 && vakje3 ==0) {
+    else if (mouseX > 700 && mouseX < 700 + 125 && mouseY > 70 && mouseY < 70 + 125 && vakje3 == 0) {
       vakje3 = beurt
       wisselBeurtNaarVolgendeSpeler();
     }
     // tweede rij
-    else if (mouseX > 425 && mouseX < 425 + 125 && mouseY > 215 && mouseY < 215 + 125 && vakje4 ==0) {
+    else if (mouseX > 425 && mouseX < 425 + 125 && mouseY > 215 && mouseY < 215 + 125 && vakje4 == 0) {
       vakje4 = beurt
       wisselBeurtNaarVolgendeSpeler();
     }
-    else if (mouseX > 563 && mouseX < 563 + 125 && mouseY > 215 && mouseY < 215 + 125 && vakje5 ==0) {
+    else if (mouseX > 563 && mouseX < 563 + 125 && mouseY > 215 && mouseY < 215 + 125 && vakje5 == 0) {
       vakje5 = beurt
       wisselBeurtNaarVolgendeSpeler();
     }
-    else if (mouseX > 700 && mouseX < 700 + 125 && mouseY > 215 && mouseY < 215 + 125 && vakje6 ==0) {
+    else if (mouseX > 700 && mouseX < 700 + 125 && mouseY > 215 && mouseY < 215 + 125 && vakje6 == 0) {
       vakje6 = beurt
       wisselBeurtNaarVolgendeSpeler();
       //derde rij
     }
-    else if (mouseX > 425 && mouseX < 425 + 125 && mouseY > 360 && mouseY <360 + 125 && vakje7 ==0) {
+    else if (mouseX > 425 && mouseX < 425 + 125 && mouseY > 360 && mouseY < 360 + 125 && vakje7 == 0) {
       vakje7 = beurt
       wisselBeurtNaarVolgendeSpeler();
     }
-    else if (mouseX > 563 && mouseX < 563 + 125 && mouseY > 360 && mouseY < 360 + 125 && vakje8 ==0) {
+    else if (mouseX > 563 && mouseX < 563 + 125 && mouseY > 360 && mouseY < 360 + 125 && vakje8 == 0) {
       vakje8 = beurt
       wisselBeurtNaarVolgendeSpeler();
     }
-    else if (mouseX > 700 && mouseX < 700 + 125 && mouseY > 360 && mouseY < 360 + 125 && vakje9 ==0) {
+    else if (mouseX > 700 && mouseX < 700 + 125 && mouseY > 360 && mouseY < 360 + 125 && vakje9 == 0) {
       vakje9 = beurt
       wisselBeurtNaarVolgendeSpeler();
     }
   }
-if( vakje1 == 1 && vakje2 == 1 && vakje3 == 1 ){
-line(30,30,30,30);
-fill(0,0,0);
-text("you win",30,30);
-}
-
 
 }
 
